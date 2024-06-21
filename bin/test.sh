31fd2c2b113f3927fa0398a262f18d5ae4909721
Ed Morley <501702+edmorley@users.noreply.github.com>
2024-06-21 11:11:11 +0100

    Stop installing unnecessary packages (#20)
    - Trims the APT package list for Heroku-20 and Heroku-22 to match
      the packages list used for Heroku-24.
    - Uses `--no-install-recommends` to avoid pulling in unnecessary
      transitive dependencies.
    - Now outputs the size of the app dir when running tests (plus removes
      test containers after they exit).
    
    The tests added in #15 ensure that there are no missing dynamically
    linked libraries, and that Chrome + Chromedriver can start.
    
    This reduces the uncompressed slug size impact of this buildpack as follows:
    - Heroku-20: 442 MB -> 350 MB
    - Heroku-22: 426 MB -> 351 MB
    - Heroku-24: 381 MB -> 353 MB
    
    Fixes #17.

--- bin/test.sh
+++ bin/test.sh
@@ -11,13 +11,16 @@ docker build --progress=plain --build-arg="STACK_VERSION=${STACK_VERSION}" -t he
 # Note: All of the container commands must be run via a login bash shell otherwise the profile.d scripts won't be run.
 
 # Check the profile.d scripts correctly added the binaries to PATH.
-docker run heroku-buildpack-chrome-for-testing bash -l -c 'chrome --version'
-docker run heroku-buildpack-chrome-for-testing bash -l -c 'chromedriver --version'
+docker run --rm heroku-buildpack-chrome-for-testing bash -l -c 'chrome --version'
+docker run --rm heroku-buildpack-chrome-for-testing bash -l -c 'chromedriver --version'
 
 # Check that there are no missing dynamically linked libraries.
-docker run heroku-buildpack-chrome-for-testing bash -l -c 'ldd $(which chrome)'
-docker run heroku-buildpack-chrome-for-testing bash -l -c 'ldd $(which chromedriver)'
+docker run --rm heroku-buildpack-chrome-for-testing bash -l -c 'ldd $(which chrome)'
+docker run --rm heroku-buildpack-chrome-for-testing bash -l -c 'ldd $(which chromedriver)'
 
 # Check Chrome can fully boot in both new and old headless modes.
-docker run heroku-buildpack-chrome-for-testing bash -l -c 'chrome --no-sandbox --headless=new --screenshot https://google.com'
-docker run heroku-buildpack-chrome-for-testing bash -l -c 'chrome --no-sandbox --headless=old --screenshot https://google.com'
+docker run --rm heroku-buildpack-chrome-for-testing bash -l -c 'chrome --no-sandbox --headless=new --screenshot https://google.com'
+docker run --rm heroku-buildpack-chrome-for-testing bash -l -c 'chrome --no-sandbox --headless=old --screenshot https://google.com'
+
+# Display a size breakdown of the directories added by the buildpack to the app.
+docker run --rm heroku-buildpack-chrome-for-testing bash -l -c 'du --human-readable --max-depth=1 /app'
